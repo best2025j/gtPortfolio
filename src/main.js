54484eafd4ce9ca470d670d94ec4b9bdd07c7fd9
Boniface Audu <audugodstime95@gmail.com>
2024-11-23 16:43:49 +0100

    aos added

--- src/main.js
+++ src/main.js
@@ -1,25 +1,36 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router';
-import './assets/css/app.css';
-import BackToTop from 'vue-backtotop';
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import "./assets/css/app.css";
+import BackToTop from "vue-backtotop";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
-const feather = require('feather-icons');
+const app = createApp(App);
+
+app.mount("#app");
+
+// Initialize AOS
+AOS.init({
+  duration: 1200, // Animation duration
+  offset: 200, // Offset from the trigger point
+  easing: "ease-in-out", // Easing function
+  once: true, // Whether animation should happen only once
+});
+
+const feather = require("feather-icons");
 feather.replace();
 
-createApp(App)
-	.use(router)
-	.use(BackToTop)
-	.mount('#app');
+createApp(App).use(router).use(BackToTop).mount("#app");
 
-const appTheme = localStorage.getItem('theme');
+const appTheme = localStorage.getItem("theme");
 
 // Check what is the active theme and change theme when user clicks on the theme button in header.
 if (
-	appTheme === 'dark' &&
-	document.querySelector('body').classList.contains('app-theme')
+  appTheme === "dark" &&
+  document.querySelector("body").classList.contains("app-theme")
 ) {
-	document.querySelector('body').classList.add('bg-primary-dark');
+  document.querySelector("body").classList.add("bg-primary-dark");
 } else {
-	document.querySelector('body').classList.add('bg-secondary-light');
+  document.querySelector("body").classList.add("bg-secondary-light");
 }
--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -43,24 +43,77 @@ export default {
   <div
     class="container mx-auto flex-col-reverse md:flex-row py-5 md:py-10 md:mt-10"
   >
-    <div class="h-full w-1/2 border rounded-3xl">
-      hello gt
+    <div>
+      <div class="z-20 relative left-24">
+        <div class="flex relative top-48 right-20">
+          <img
+            src="../assets/svgs/custom/freepic-arrow.svg"
+            alt=""
+            class="relative top-40"
+          />
+          <img
+            src="../assets/svgs/custom/freepic-speech.svg"
+            alt=""
+            class="relative top-24 left-5"
+          />
+        </div>
+        <img
+          src="../assets/svgs/custom/freepic-arrowUp.svg"
+          alt=""
+          data-aos="fade-right"
+          data-aos-delay="300"
+          data-aos-duration="1000"
+          class="relative top-56 left-28"
+        />
+        <div>
+          <img
+            src="../assets/svgs/custom/freepic-character.svg"
+            alt=""
+            data-aos="fade-right"
+            class="relative top-20 z-20"
+          />
+          <img
+            src="../assets/svgs/custom/freepic-hand.svg"
+            alt=""
+            data-aos="zoom-in"
+          />
+        </div>
+        <img
+          src="../assets/svgs/custom/freepic-arrowDown.svg"
+          alt=""
+          data-aos="fade-right"
+          data-aos-delay="100"
+          data-aos-duration="700"
+          class="relative bottom-[6.5rem] right-[4.8rem] z-10"
+        />
+        <div class="relative bottom-44">
+          <div class="flex items-center relative bottom-10">
+            <img
+              src="../assets/svgs/custom/freepic-plant.svg"
+              alt=""
+              class="relative -left-24 -top-8"
+            />
+            <img src="../assets/svgs/custom/freepic-coin.svg" alt="" />
+            <img
+              src="../assets/svgs/custom/freepic-bar.svg"
+              alt=""
+              class="relative -top-16 left-4"
+            />
+          </div>
+        </div>
+      </div>
 
-      <div>
-        <span>
-          <img src="../assets/svgs/custom/freepic-arrow.svg" alt="" />
-          <img src="../assets/svgs/custom/freepic-bar.svg" alt="" />
-          <img src="../assets/svgs/custom/freepic-arrowUp.svg" alt="" />
-          <img src="../assets/svgs/custom/freepic-character.svg" alt="" />
-          <img src="../assets/svgs/custom/freepic-hand.svg" alt="" />
-          <img src="../assets/svgs/custom/freepic-arrowDown.svg" alt="" />
-          <img src="../assets/svgs/custom/freepic-coin.svg" alt="" />
-          <img src="../assets/svgs/custom/freepic-speech.svg" alt="" />
-          <img src="../assets/svgs/custom/freepic-plant.svg" alt="" />
-          <img src="../assets/svgs/custom/freepic-floor.svg" alt="" />
-        </span>
+      <div class="relative">
+        <img
+          src="../assets/svgs/custom/freepic-floor.svg"
+          alt=""
+          class="absolute bottom-[16.5rem]"
+          width="356.64"
+          height="204.32"
+        />
       </div>
     </div>
+
     <div
       class="container mx-auto flex flex-col-reverse md:flex-row py-5 md:py-10 md:mt-10"
     >
